fbfe950c715702f7bacd9ceb00d25cf47ae15a82
Chowhan-Sehwag <chowhansehwag0@gmail.com>
2026-05-23 14:36:33 +0530

    Update 'Investor Relations' to 'Investor Relationships'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
         <div className="sideMenu">
           <img src={blackLogo} alt="logo" />
           <div className="innerMenu">
-            <span>Investor Relations</span>
+            <span>Investor Relationships</span>
             <span>Add restaurants</span>
             <span>Log In</span>
             <span>Sign Up</span>
